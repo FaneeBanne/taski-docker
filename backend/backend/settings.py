d1012f27fc6b205bddef2c2e3b261b12d374b0de
Nikita Akulin <bmww52@yandex.ru>
2026-01-04 14:15:57 +0300

    add ip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taski.3utilities.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taski.3utilities.com', '158.160.77.11']
 
 
 # Application definition
